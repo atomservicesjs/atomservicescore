b239ee20005e43c94d0f9de2cbd5f40f077d77c7
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-21 12:22:58 +0700

    feat(IQueryHandler): change query

--- src/Queries/Querying.ts
+++ src/Queries/Querying.ts
@@ -1,3 +1,4 @@
 import { IQuery } from "../IQuery";
 
-export type Querying<Payloads = any, QueryResult = any> = (query: IQuery<Payloads>) => Promise<QueryResult>;
+export type Querying<Payloads = any, QueryResult = any> = (query: IQuery<Payloads>, type: string) =>
+  Promise<QueryResult>;
