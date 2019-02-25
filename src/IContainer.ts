a5b80713cfd7ae0b4b30a38d4add2b9551e0b157
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-25 16:09:39 +0700

    feat(IContainer): add service()

--- src/IContainer.ts
+++ src/IContainer.ts
@@ -3,7 +3,9 @@ import { IService } from "./IService";
 import { IServiceBootstrapable } from "./Services/IServiceBootstrapable";
 
 export interface IContainer extends IServiceBootstrapable {
-  name: () => string;
-  configs: () => IContainerConfigs;
-  registerService: (service: IService) => void;
+  name(): string;
+  configs(): IContainerConfigs;
+  registerService(service: IService): void;
+  service(): IService[];
+  service(name: string): IService | undefined;
 }
