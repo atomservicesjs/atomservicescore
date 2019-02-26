66fb464548c17793a69a6c7892b870ac57953b06
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-26 15:58:28 +0700

    release: v.0.3.0-alpha.4

--- src/EventStreams/EventSubscribing.ts
+++ src/EventStreams/EventSubscribing.ts
@@ -3,6 +3,6 @@ import { EventProcess } from "./EventProcess";
 
 export type EventSubscribing = (
   on: { name: string; type: string; scope: string; level: EventLevel; },
-  to: { channel?: string; type: string; scope: string; },
+  to: { channel: string; type: string; scope: string; },
   process: EventProcess,
 ) => void;
