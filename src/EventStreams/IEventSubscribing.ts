788d4ff1163c69ad75bf469f15f853a367a89599
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-26 19:10:44 +0700

    refactor

--- src/EventStreams/IEventSubscribing.ts
+++ src/EventStreams/IEventSubscribing.ts
@@ -1,11 +1,11 @@
+import { IEvent } from "../IEvent";
 import { EventChannel } from "./EventChannel";
 import { EventLevel } from "./EventLevel";
-import { EventProcess } from "./EventProcess";
 
 export interface IEventSubscribing {
   subscribe: (
     on: { name: string; type: string; scope: string; level: EventLevel; },
     to: { channel: EventChannel; type: string; scope: string; },
-    process: EventProcess,
+    process: (event: IEvent, ack: () => void) => void,
   ) => Promise<void>;
 }
