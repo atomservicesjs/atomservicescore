ae45f6f09ffd776be9e49819638492fa16fe941b
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-26 16:54:07 +0700

    release: v.0.3.0-alpha.11

--- _scripts/aftertests.js
+++ _scripts/aftertests.js
@@ -3,3 +3,5 @@ const shell = require('shelljs');
 shell.rm('-rf', '.nyc_output');
 shell.rm('-rf', './lib/**/*.spec.js');
 shell.rm('-rf', './lib/**/*.spec.js.map');
+
+shell.rm('-rf', './typings/**/*.spec.d.ts');
