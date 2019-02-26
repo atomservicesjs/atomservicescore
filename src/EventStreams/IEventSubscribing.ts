433c304f71785caea406b7f60b11ff6a7a915c2f
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-26 18:53:42 +0700

    release: v.0.3.0-alpha.16

--- src/EventStreams/IEventSubscribing.ts
+++ src/EventStreams/IEventSubscribing.ts
@@ -1,10 +1,11 @@
+import { EventChannel } from "./EventChannel";
 import { EventLevel } from "./EventLevel";
 import { EventProcess } from "./EventProcess";
 
 export interface IEventSubscribing {
   subscribe: (
     on: { name: string; type: string; scope: string; level: EventLevel; },
-    to: { channel: string; type: string; scope: string; },
+    to: { channel: EventChannel; type: string; scope: string; },
     process: EventProcess,
   ) => Promise<void>;
 }
