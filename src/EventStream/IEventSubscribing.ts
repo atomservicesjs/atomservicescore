a396fd5d6a93297f034d66d6c04db675dbe692fb
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-06-24 19:04:17 +0700

    feat(ServiceEventStream)

--- src/EventStream/IEventSubscribing.ts
+++ src/EventStream/IEventSubscribing.ts
@@ -6,7 +6,7 @@ export interface IEventSubscribing {
   subscribe: (
     on: { name: string; type: string; scope: string; level: StreamLevel; },
     to: { channel: EventChannel; type: string; scope: string; },
-    process: (event: IEvent, processAck: () => void) => Promise<void>,
+    process: (event: IEvent, processAck: () => Promise<void>) => Promise<void>,
   ) => Promise<{
     on: { name: string; type: string; scope: string; level: StreamLevel; };
     to: { channel: EventChannel; type: string; scope: string; };
--- src/IServiceEventStream.ts
+++ src/IServiceEventStream.ts
@@ -10,5 +10,5 @@ export interface IServiceEventStream {
   type: () => string;
   dispatch: (event: IEvent, isReplay?: boolean) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
   registerHandler: (handler: IEventHandler, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
-  registerReaction: (reaction: IReaction, process: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  registerReaction: (reaction: IReaction, react: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
 }
