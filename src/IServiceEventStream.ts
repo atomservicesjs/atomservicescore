494e91dcab2ee44162cfe57328f8966440a4cd9a
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-07-01 16:03:41 +0700

    feat(ServiceEventStream)

--- src/IServiceEventStream.ts
+++ src/IServiceEventStream.ts
@@ -2,13 +2,11 @@ import { EventProcess } from "./EventStream/EventProcess";
 import { EventReact } from "./EventStream/EventReact";
 import { StreamLevel } from "./EventStream/StreamLevel";
 import { IEvent } from "./IEvent";
-import { IEventHandler } from "./IEventHandler";
-import { IReaction } from "./IReaction";
 
 export interface IServiceEventStream {
   scope: () => string;
   type: () => string;
   dispatch: (event: IEvent, isReplay?: boolean) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
-  registerHandler: (handler: IEventHandler, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
-  registerReaction: (reaction: IReaction, react: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  registerEventProcess: (on: { name: string; }, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  registerEventReact: (on: { scope: string; type: string; name: string; }, react: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
 }
