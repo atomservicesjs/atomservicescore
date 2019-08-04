6a8ff85cc456983f169a985ca225b8092443bf52
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-04 14:34:09 +0700

    feat(eventstream)

--- src/IServiceEventStream.ts
+++ src/IServiceEventStream.ts
@@ -6,7 +6,11 @@ import { IEvent } from "./IEvent";
 export interface IServiceEventStream {
   scope: () => string;
   type: () => string;
-  dispatch: (event: IEvent, isReplay?: boolean) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  dispatch: (
+    event: IEvent,
+    metadata?: {
+      isReplay: boolean;
+    }) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
   directTo: (ref: string, data: any) => Promise<any>;
   listenTo: (ref: string, listener: (data: any) => void) => Promise<any>;
   registerEventProcess: (on: { name: string; }, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
