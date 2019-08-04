7ce42a38f0d823db143d864bd188577a97706a0b
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-04 14:28:31 +0700

    feat(event stream)

--- src/EventStream/IEventPublishing.ts
+++ src/EventStream/IEventPublishing.ts
@@ -2,6 +2,11 @@ import { IEvent } from "../IEvent";
 import { StreamLevel } from "./StreamLevel";
 
 export interface IEventPublishing {
-  publish: (event: IEvent, on: { scope: string; level: StreamLevel; }, isReplay?: boolean) =>
+  publish: (
+    event: IEvent,
+    on: { scope: string; level: StreamLevel; },
+    metadata?: {
+      isReplay: boolean;
+    }) =>
     Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
 }
