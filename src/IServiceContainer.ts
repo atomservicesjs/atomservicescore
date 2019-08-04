12766f24dc7f76ba9a23dfde22ffd60408c10af3
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-04 11:31:36 +0700

    feat(ServiceContainer)

--- src/IServiceContainer.ts
+++ src/IServiceContainer.ts
@@ -1,3 +1,8 @@
+import { IService } from "./IService";
+
 export interface IServiceContainer {
+  connect: () => Promise<void>;
+  readonly isConnected: boolean;
+  registerService: (service: IService) => Promise<IService>;
   scope: () => string;
 }
