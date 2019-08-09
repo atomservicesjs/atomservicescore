7b0ca85515a920616a4dc2794c091e05d764b73b
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-09 14:02:04 +0700

    feat(ServiceContext)

--- src/Service/IServiceContext.ts
+++ src/Service/IServiceContext.ts
@@ -1,4 +1,4 @@
-import { EventProcess, EventReact } from "../Core/EventStream";
+// import { EventProcess, EventReact } from "../Core/EventStream";
 import { StreamLevel } from "../Core/EventStream/StreamLevel";
 import { IEvent } from "../IEvent";
 
@@ -10,6 +10,6 @@ export interface IServiceContext<AggregateID = any, EventID = any> {
   dispatch: (event: IEvent) => Promise<{ level: StreamLevel; name: string; scope: string; type: string; }>;
   directTo: (ref: string, data: any) => Promise<any>;
   listenTo: (ref: string, listener: (data: any) => any) => Promise<any>;
-  registerEventProcess: (on: { name: string; }, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
-  registerEventReact: (on: { scope: string; type: string; name: string; }, react: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  // registerEventProcess: (on: { name: string; }, process: EventProcess) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
+  // registerEventReact: (on: { scope: string; type: string; name: string; }, react: EventReact) => Promise<{ name: string; type: string; scope: string; level: StreamLevel; }>;
 }
