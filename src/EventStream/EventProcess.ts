55d27cab82859ef04451bc3a2e2e2eb47bdf4207
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-21 10:56:06 +0700

    release: v.0.6.0-alpha.9

--- src/EventStream/EventProcess.ts
+++
@@ -1,8 +0,0 @@
-import { IEvent } from "../IEvent";
-import { IStreamMetadata } from "./IStreamMetadata";
-
-export type EventProcess<Event extends IEvent = IEvent> = (
-  event: Event,
-  metadata: IStreamMetadata,
-  processAck: () => Promise<void>,
-) => Promise<void>;
--- src/EventStream/EventReact.ts
+++
@@ -1,8 +0,0 @@
-import { IEvent } from "../IEvent";
-import { IStreamMetadata } from "./IStreamMetadata";
-
-export type EventReact<Event extends IEvent = IEvent> = (
-  event: Event,
-  metadata: IStreamMetadata,
-  processAck: () => Promise<void>,
-) => Promise<void>;
