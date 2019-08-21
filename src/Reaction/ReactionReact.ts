88022f0dd57ba7e62e2d3b8decebdcb0c3f352b4
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-08-21 09:15:28 +0700

    feat(Reaction)

--- src/Reaction/ReactionReact.ts
+++ src/Reaction/ReactionReact.ts
@@ -2,4 +2,5 @@ import { IStreamMetadata } from "../EventStream";
 import { IEvent } from "../IEvent";
 import { IServiceContext } from "../Service/IServiceContext";
 
-export type ReactionReact<Event extends IEvent = IEvent> = (event: Event, scope: string, context: IServiceContext, metadata: IStreamMetadata) => Promise<void>;
+export type ReactionReact<Event extends IEvent = IEvent> =
+  (event: Event, context: IServiceContext, metadata: IStreamMetadata) => Promise<void>;
