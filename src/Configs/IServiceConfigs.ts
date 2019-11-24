94826ab9af713b30398f720a37edeb3fb20e462d
Panit Tuangsuwan <panit@Panits-MacBook-Pro-2.local>
2019-11-24 09:52:03 +0700

    release: v.0.6.0-alpha.22

--- src/Configs/IServiceConfigs.ts
+++ src/Configs/IServiceConfigs.ts
@@ -1,15 +1,7 @@
-import { StreamLevel } from "../EventStream/StreamLevel";
+// import { StreamLevel } from "../EventStream/StreamLevel";
 import { IServiceConfigsLevels } from "./IServiceConfigsLevels";
 
 export interface IServiceConfigs extends IServiceConfigsLevels {
   scope: string;
   type: string;
-  subscribe?: {
-    [type: string]: StreamLevel[];
-  };
-  reaction?: {
-    [scope: string]: {
-      [type: string]: string[];
-    };
-  };
 }
