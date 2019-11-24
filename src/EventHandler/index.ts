acf35a45a1c0235530f81f07354c43f50e3067e4
Panit Tuangsuwan <panit@Panits-MacBook-Pro-2.local>
2019-11-24 14:11:55 +0700

    feat(EventHandler)

--- src/EventHandler/index.ts
+++ src/EventHandler/index.ts
@@ -1,3 +1,4 @@
 export { AnyProcessResult } from "./AnyProcessResult";
+export { AnyState } from "./AnyState";
 export { EventProcessHandle } from "./EventProcessHandle";
 export { EventProcessEffectHandle } from "./EventProcessEffectHandle";
--- src/IEventHandler.ts
+++ src/IEventHandler.ts
@@ -1,9 +1,8 @@
 import { AnyProcessResult } from "./EventHandler/AnyProcessResult";
+import { AnyState } from "./EventHandler/AnyState";
 import { EventProcessEffectHandle } from "./EventHandler/EventProcessEffectHandle";
 import { EventProcessHandle } from "./EventHandler/EventProcessHandle";
 import { IEvent } from "./IEvent";
-
-type AnyState = any;
 
 export interface IEventHandler<Event extends IEvent = IEvent, ProcessResult = AnyProcessResult, State = AnyState> {
   name: string;
