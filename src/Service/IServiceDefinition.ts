7ac6c32d7fae56dfe50b750cef1b634d52e9165e
Panit Tuangsuwan <panit@Panits-MacBook-Pro-2.local>
2019-11-26 09:44:46 +0700

    release: v.0.6.0-alpha.29

--- src/Service/IServiceDefinition.ts
+++ src/Service/IServiceDefinition.ts
@@ -1,6 +1,12 @@
 import { IEventHandler } from "../IEventHandler";
+import { IEventStores } from "../IEventStores";
+import { IEventStream } from "../IEventStream";
+import { IServiceConfigs } from "./IServiceConfigs";
 
 export interface IServiceDefinition {
   type: string;
+  configs?: IServiceConfigs;
   EventHandlers?: IEventHandler[];
+  EventStores?: IEventStores;
+  EventStream?: IEventStream;
 }
--- src/Service/IServiceStreamLevel.ts
+++ src/Service/IServiceStreamLevel.ts
@@ -1,6 +1,5 @@
 import { StreamLevel } from "../EventStream/StreamLevel";
 
 export interface IServiceStreamLevel {
-  type: () => string;
   level: (name: string) => StreamLevel;
 }
