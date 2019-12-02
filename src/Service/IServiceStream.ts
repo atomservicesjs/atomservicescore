f9d0076fac0f29e84df81247b494f531bf39d6bb
Panit Tuangsuwan <panit@Panits-MacBook-Pro-2.local>
2019-12-02 19:16:54 +0700

    release: v.0.7.0-alpha.35

--- src/Service/IServiceStream.ts
+++ src/Service/IServiceStream.ts
@@ -2,6 +2,7 @@ import { StreamLevel } from "../EventStream/StreamLevel";
 import { EventProcessType } from "./EventProcessType";
 
 export interface IServiceStream {
+  allowNoVersion: (name: string) => boolean;
   level: (name: string) => StreamLevel;
   processType: (name: string) => EventProcessType;
 }
