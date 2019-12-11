21dc1fdc68c58a56bb89eaf1e09753e69d12d713
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-12-11 15:26:21 +0700

    release: v.0.7.0-alpha.63

--- src/IServiceContainer.ts
+++ src/IServiceContainer.ts
@@ -3,14 +3,14 @@ import { IEventStream } from "./IEventStream";
 import { IIdentifier } from "./IIdentifier";
 import { INotifier } from "./INotifier";
 import { IService } from "./IService";
-import { IStateStoresConnector } from "./IStateStoresConnector";
+import { IStateStores } from "./IStateStores";
 
 export interface IServiceContainer {
   scope: string;
   Services: IService[];
   EventStores?: IEventStores;
+  StateStores?: IStateStores;
   EventStream?: IEventStream;
   Identifier?: IIdentifier;
   Notifiers?: INotifier[];
-  StateStores?: IStateStoresConnector;
 }
