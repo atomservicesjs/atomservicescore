797e0486f6b4c7acc443ae5b8716c60c346a5bcd
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-12-11 17:06:43 +0700

    release: v.0.7.0-alpha.69

--- src/IEventHandler.ts
+++ src/IEventHandler.ts
@@ -1,11 +1,10 @@
 import { AnyProcessResult } from "./EventHandler/AnyProcessResult";
-import { AnyState } from "./EventHandler/AnyState";
 import { EventProcessEffectHandle } from "./EventHandler/EventProcessEffectHandle";
 import { EventProcessHandle } from "./EventHandler/EventProcessHandle";
 import { IAtomEvent } from "./IAtomEvent";
 import { IEvent } from "./IEvent";
 
-export interface IEventHandler<Event extends IAtomEvent = IEvent, ProcessResult = AnyProcessResult, State = AnyState> {
+export interface IEventHandler<Event extends IAtomEvent = IEvent, ProcessResult = AnyProcessResult> {
   name: string;
   process: EventProcessHandle<Event, ProcessResult>;
   processEffect?: EventProcessEffectHandle<Event, ProcessResult>;
