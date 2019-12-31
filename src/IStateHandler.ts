93e59ea89884cfb31c25aa3ebb0fc1fbf17a86a5
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-12-31 18:40:31 +0700

    release: v.0.7.0-alpha.77

--- src/IStateHandler.ts
+++ src/IStateHandler.ts
@@ -1,8 +1,7 @@
 import { IAtomEvent } from "./IAtomEvent";
 import { IEvent } from "./IEvent";
-import { IServiceStateStores } from "./Service/IServiceStateStores";
 
 export interface IStateHandler<Event extends IAtomEvent = IEvent> {
   name: string;
-  apply: (event: Event, Stores: IServiceStateStores) => Promise<void>;
+  apply: (event: Event) => Promise<void>;
 }
