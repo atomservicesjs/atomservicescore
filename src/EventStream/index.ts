78c6062ea3396780a7f539d42142db55de4480e0
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-01-16 15:19:12 +0700

    feat(EventStream)

--- src/EventStream/index.ts
+++ src/EventStream/index.ts
@@ -5,5 +5,6 @@ export { IEventSubscribing } from "./IEventSubscribing";
 export { IReactStreamProcesses } from "./IReactStreamProcesses";
 export { IStreamMetadata } from "./IStreamMetadata";
 export { IStreamPublishOn } from "./IStreamPublishOn";
+export { ProcessTypes } from "./ProcessTypes";
 export { StreamLevel } from "./StreamLevel";
 export { StreamProcessing } from "./StreamProcessing";
