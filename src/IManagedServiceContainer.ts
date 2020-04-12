897871e32fdacdba07798b13c53780996fd23b00
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-04-12 21:25:12 +0700

    release: v.0.8.0-alpha.12

--- src/IManagedServiceContainer.ts
+++ src/IManagedServiceContainer.ts
@@ -3,8 +3,12 @@ import { ICommand } from "./ICommand";
 import { IManagedService } from "./IManagedService";
 
 export interface IManagedServiceContainer {
-  connect: () => Promise<void>;
-  composeDispatch: (type: string) => (command: ICommand, listening?: (data: any) => void) => Promise<IDispatchResult>;
-  dispatch: (type: string, command: ICommand, listening?: (data: any) => void) => Promise<IDispatchResult>;
-  service: (type: string) => IManagedService;
+  connect: () =>
+    Promise<void>;
+  composeDispatch: (type: string, options?: { isAutoConnect?: boolean; }) =>
+    (command: ICommand, listening?: (data: any) => void) => Promise<IDispatchResult>;
+  dispatch: (type: string, command: ICommand, listening?: (data: any) => void) =>
+    Promise<IDispatchResult>;
+  service: (type: string) =>
+    IManagedService;
 }
