11b7319de502029758dd8a6166c9467db55fffee
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-05-02 13:18:27 +0700

    release: v.0.8.0-alpha.17

--- src/IEvent.ts
+++ src/IEvent.ts
@@ -6,6 +6,30 @@ export interface IEvent<Payloads = any, EventID = any, AggregateID = any, Create
   payloads: Payloads;
   _createdAt: Date;
   _createdBy: CreatedBy;
-  _version?: number;
+  _version: number | undefined;
+  [key: string]: any;
+}
+
+export interface IEventWithVersioning<Payloads = any, EventID = any, AggregateID = any, CreatedBy = any> {
+  _id: EventID;
+  type: string;
+  name: string;
+  aggregateID: AggregateID;
+  payloads: Payloads;
+  _createdAt: Date;
+  _createdBy: CreatedBy;
+  _version: number;
+  [key: string]: any;
+}
+
+export interface IEventWithoutVersioning<Payloads = any, EventID = any, AggregateID = any, CreatedBy = any> {
+  _id: EventID;
+  type: string;
+  name: string;
+  aggregateID: AggregateID;
+  payloads: Payloads;
+  _createdAt: Date;
+  _createdBy: CreatedBy;
+  _version: undefined;
   [key: string]: any;
 }
