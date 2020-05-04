ef2d6288cd71bb70fe725e6e32d6cf1fff3b419d
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-05-04 20:49:52 +0700

    release: v.0.8.0-alpha.27

--- src/ISFComponents.ts
+++ src/ISFComponents.ts
@@ -3,17 +3,15 @@ import { ICommandHandler } from "./ICommandHandler";
 import { IEvent } from "./IEvent";
 import { IEventHandler } from "./IEventHandler";
 
-export interface ISFComponents<
-  Event extends IEvent = IEvent,
-  Command extends ICommand = ICommand<Event["payloads"], Event["_createdBy"]>,
-  Commander extends (...props: any[]) => Command = (props: { [prop in keyof Command["payloads"]]: Command["payloads"][prop]; } & {
+export interface ISFComponents<Event extends IEvent = IEvent, Command extends ICommand = ICommand<Event["payloads"], Event["_createdBy"]>, ProcessResult = any> {
+  Commander: (props: {
+    [prop in keyof Command["payloads"]]: Command["payloads"][prop];
+  } & {
     aggregateID?: Event["aggregateID"];
     _createdBy?: Command["_createdBy"];
     _version?: Command["_version"];
     [key: string]: any;
-  }, ...others: any[]) => Command,
-  ProcessResult = any> {
-  Commander: Commander;
+  }) => Command;
   CommandHandler: ICommandHandler<Command, Event>;
   EventHandler: IEventHandler<Event, ProcessResult>;
 };
